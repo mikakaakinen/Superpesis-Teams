d24b5c7b503ed92f0e987fa5e725d931113be0d4
Mika Kaakinen <mika.kaakinen@gmail.com>
2022-12-22 13:11:59 +0200

    Final commit

--- server.js
+++ server.js
@@ -1437,6 +1437,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
